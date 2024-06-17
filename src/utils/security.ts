c6e94a69eba26a46ae13996f2b55c7d6a022caa1
majorek31 <m4y0r.offical@gmail.com>
2024-06-17 22:04:28 +0200

    feat: add compareHash

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -4,3 +4,10 @@ export async function generatePasswordHash(password: string): Promise<string> {
   const hashedPassword = await bcrypt.hash(password, 10);
   return hashedPassword;
 }
+
+export async function compareHash(
+  password: string,
+  hash: string,
+): Promise<boolean> {
+  return await bcrypt.compare(password, hash);
+}
