c1692675e8cafce6680f1a7d67b41f7fe4a458e7
majorek31 <m4y0r.offical@gmail.com>
2024-06-18 22:11:40 +0200

    feat: add createAccessToken

--- src/utils/security.ts
+++ src/utils/security.ts
@@ -1,4 +1,8 @@
 import bcrypt from "bcrypt";
+import type { AccessToken } from "../models/auth/accessToken";
+import type { User } from "@prisma/client";
+import { sign } from "hono/jwt";
+import { JWTPayload } from "hono/utils/jwt/types";
 
 export async function generatePasswordHash(password: string): Promise<string> {
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -10,4 +14,28 @@ export async function compareHash(
   hash: string,
 ): Promise<boolean> {
   return await bcrypt.compare(password, hash);
+}
+
+interface TokenPayload extends JWTPayload {
+  email: string;
+  role: string;
+}
+
+export async function createAccessToken(user: User): Promise<AccessToken> {
+  const secret = process.env.JWT_SECRET;
+  const expirationSeconds = parseInt(process.env.JWT_EXPIRES);
+  if (!secret) throw new Error("JWT_SECRET is null");
+  const expirationDate = Math.floor(Date.now() / 1000) + expirationSeconds;
+  const token = await sign(
+    {
+      email: user.email,
+      exp: expirationDate,
+      role: "test-role",
+    } as TokenPayload,
+    secret,
+  );
+  return {
+    token,
+    expires: expirationDate,
+  };
 }
